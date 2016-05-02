43e7cc0ca9898988ab62ab257457f67d6252b783
Cristina Costa <cncosta25@gmail.com>
2016-05-02 15:12:18 -0500

    directors view done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,49 @@
 Rails.application.routes.draw do
-  
+
+# Route for default domain:
+
+#get("/", { :controller => "movies", :action => "index"})
+
+
+# Director routes
+# READ
+  get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/:id", { :controller => "directors", :action => "show"})
+
+
+# DELETE
+
+# CREATE
+
+# UPDATE
+
+
+# Actor routes
+# READ
+
+  get("/actors", { :controller => "actors", :action => "index"})
+
+  get("/actors/:id", { :controller => "actors", :action => "show"})
+
+# DELETE
+
+# CREATE
+
+# UPDATE
+
+# Movie routes
+# READ
+
+  get("/movies", { :controller => "movies", :action => "index"})
+
+  get("/movies/:id", { :controller => "movies", :action => "show"})
+
+
+# DELETE
+
+# CREATE
+
+# UPDATE
+
 end
