285909f9671a110802925b2d4c9d850a9704c59c
Cristina Costa <cncosta25@gmail.com>
2016-05-02 16:24:21 -0500

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,13 @@ Rails.application.routes.draw do
 
 # CREATE
 
+  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
+  get('/create_director', { :controller => 'directors', :action => 'create_row' })
+
 # UPDATE
 
+  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
 
 # Actor routes
 # READ
