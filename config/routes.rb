310c848d5994cf798563c0b89fe9ec0e5b743439
Leland Brewster <jlbrew3@users.noreply.github.com>
2016-05-03 17:12:15 -0500

    director index working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+
+  get('/', { :controller => 'directors', :action => 'index' })
+
+  # READ
+  get('/directors', { :controller => 'directors', :action => 'index' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
+
 end
