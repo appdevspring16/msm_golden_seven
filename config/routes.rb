5f90dafb10fd99af267dd1df7114eb54f31cae61
jmunoz0 <josemunoz@Joses-Air.zyxel.com>
2016-05-04 00:37:24 -0500

    done with movies section

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,53 @@
 Rails.application.routes.draw do
-  
+
+
+#movies
+get('/', {:controller=> 'movies', :action=>'index'})
+#Create movies
+get('/movies/new_form', {:controller=>'movies', :action=>'new_form'})
+get('/create_movie', {:controller=>'movies', :action=>'create_row'})
+# get('/movies/new_form', {:controller=>'movies', :action=>'new_form'})
+#Read movies
+get('/movies', {:controller=>'movies', :action=>'index'})
+get('/movies/:id', {:controller=>'movies', :action=>'show'})
+#Update movies
+get('/movies/:id/edit_form',{:controller=>'movies', :action=>'edit_form'})
+get('/update_movie/:id',{:controller=>'movies', :action=>'update_row'})
+#Destroy movies
+get('/delete_movie/:id',{:controller=>'movies', :action=>'destroy'})
+
+
+##################################
+
+#directors
+
+#Create directors
+
+
+#Read directors
+
+
+#Update directors
+
+
+#Destroy directors
+
+
+
+
+##################################
+
+#actors
+
+#Create actors
+
+
+#Read actors
+
+
+#Update actors
+
+
+#Destroy actors
+
 end
