b2408517cf484b2f2b86c37397b84705ea27c36c
jmunoz0 <josemunoz@Joses-Air.zyxel.com>
2016-05-04 03:07:29 -0500

    good progress with directors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,7 @@ class MoviesController<ApplicationController
   end
 
 def show
-  @movie=Movie.find_by({:id=>params[:id]})
+@movie=Movie.find_by({:id=>params[:id]})
 end
 
 def new_form
--- config/routes.rb
+++ config/routes.rb
@@ -22,16 +22,19 @@ get('/delete_movie/:id',{:controller=>'movies', :action=>'destroy'})
 #directors
 
 #Create directors
-
+get('/directors/new_form', {:controller=>'directors', :action=>'new_form'})
+get('/create_director', {:controller=>'directors', :action=>'create_row'})
 
 #Read directors
-
+get('/directors', {:controller=>'directors', :action=>'index'})
+get('/directors/:id', {:controller=>'directors', :action=>'show'})
 
 #Update directors
-
+get('/directors/:id/edit_form',{:controller=>'directors', :action=>'edit_form'})
+get('/update_director/:id',{:controller=>'directors', :action=>'update_row'})
 
 #Destroy directors
-
+get('/delete_director/:id',{:controller=>'directors', :action=>'destroy'})
 
 
 
