fb0ee79d80de2cfdcc6d80d7fe84d71e0aa69cd3
Kenny Moyer <kmoyer@users.noreply.github.com>
2016-05-04 20:23:26 -0400

    kenny-moyer-commit-directors-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
-  
+
+#Routes to main page
+get("/",                  { :controller => "movies", :action => "index"})
+
+
+#Routes to CREATE DIRECTORS
+get("/directors/new",     { :controller => "directors", :action => "new_form" })
+get("/create_director",   { :controller => "directors", :action => "create_row"})
+
+
+#Routes to READ DIRECTORS
+get("/directors",         { :controller => "directors", :action => "index"})
+get("/directors/:id"      { :controller => "directors", :action => "show_details"})
+
+
+
+#Routes to UPDATE DIRECTORS
+
+
+
+
+#Routes to DESTROY DIRECTORS
+
+
+
+
+
 end
