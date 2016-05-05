d468992abac09d7a75e3797bade4fc0d3266e29f
hbarton25 <hbarton@users.noreply.github.com>
2016-05-05 16:35:58 -0500

    haley-barton-msm
    -functionality complete for movies
    -need to replicate for directors and actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  
+
+  #Route for home page:
+  get('/', { :controller => 'movies', :action => 'index'})
+
+  #Movie routes:
+
+    #routes to CREATE:
+    get('/movies/new_form', { :controller => 'movies', :action => 'new_form'})
+    get('/create_movie', { :controller => 'movies', :action => 'create_row'})
+
+    #routes to READ:
+      get('/movies', { :controller => 'movies', :action => 'index'})
+      get('/movies/:id', { :controller => 'movies', :action => 'show'})
+
+    #routes to EDIT:
+    get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form'})
+    get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
+
+    #routes to DELETE:
+    get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy'})
+
 end
