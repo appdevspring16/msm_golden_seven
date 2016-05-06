8d1971e27325780d0317cc4bd830dc8cc69c3fb8
Mitch <mitch.emerson@users.noreply.github.com>
2016-05-06 09:43:13 -0500

    added Show links

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
   get('/create_actor', { :controller => 'actors', :action => 'create_row' })
   #READ
   get('/actors', { :controller => 'actors', :action => 'index' })
+  get('/actors/:id', { :controller => 'actors', :action => 'show' })
   #UPDATE
   get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
   get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
@@ -45,11 +46,12 @@ Rails.application.routes.draw do
   get('/create_movie', { :controller => 'movies', :action => 'create_row' })
   #READ
   get('/movies', { :controller => 'movies', :action => 'index' })
+  get('/movies/:id', { :controller => 'movies', :action => 'show' })
   #UPDATE
   get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form' })
   get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
   #DELETE
   get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' })
-  
+
 
 end
