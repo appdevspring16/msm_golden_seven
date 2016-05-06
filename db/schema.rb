28826e9029bded4d1fb81575c6a8c342b1fd0728
Chris Jacobson <cjacobson@stanfordalumni.org>
2016-05-05 20:44:05 -0500

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506012832) do
+ActiveRecord::Schema.define(version: 20160506013337) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20160506012832) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "director_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "character_name"
+    t.integer  "movie_id"
+    t.integer  "actor_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
