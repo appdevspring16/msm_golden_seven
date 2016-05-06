0583cbed0299985c257b994ab392e950fa7ea804
Hannah <hannah.meyer@chicagobooth.edu>
2016-05-06 12:55:40 -0500

    Edit Movie finished

--- app/controllers/mdb_controller.rb
+++ app/controllers/mdb_controller.rb
@@ -12,5 +12,35 @@ class MdbController < ApplicationController
   end
 
   def show_movie
+    m = Movie.find_by({ :id => params[:id]})
+
+    @movie = m
+  end
+
+  def create_movie
+    m = Movie.new
+    m.image_url = params[:image_url]
+    m.title = params[:movie_title]
+    m.year = params[:movie_year]
+    m.duration = params[:movie_duration]
+    m.description =params[:movie_description]
+    m.save
+    redirect_to("http://localhost:3000/movies")
+  end
+
+  def edit_form_movie
+    m = Movie.find_by({ :id => params[:id]})
+    @movie = m
+  end
+
+  def update_row_movie
+    m = Movie.find_by({ :id => params[:id]})
+    @movie = m
+    m.title = params[:movie_title]
+    m.year = params[:movie_year]
+    m.duration = params[:movie_duration]
+    m.description =params[:movie_description]
+    m.save
+    redirect_to("http://localhost:3000/movies")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,12 @@ Rails.application.routes.draw do
   get("/actors/:id/edit_form",          { :controller => "mdb", :action => "edit_form_actor" })
 
   #Movie routes
+  get("/create_movie",                  { :controller => "mdb", :action => "create_movie"})
   get("/movies",                        { :controller => "mdb", :action => "movies"})
   get("/movies/new_form",               { :controller => "mdb", :action => "new_form_movie"})
   get("/movies/:id",                    { :controller => "mdb", :action => "show_movie"})
-  get("/movies/:id/edit_form",          { :controller => "mdb", :action => "edit_form_movie"})
+  get("/movies/:id/edit",              { :controller => "mdb", :action => "edit_form_movie"})
+  get("/update_movie/:id",              { :controller => "mdb", :action => "update_row_movie"})
 
 
 
