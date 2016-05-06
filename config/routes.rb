5b1bfe8a86b13016a5c2c33814e20bf563eb6d63
Matthew Sidley <msidley@chicagobooth.edu>
2016-05-06 11:17:59 -0500

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ get('/delete_:type/:id', { :controller => 'all', :action => 'delete' })
 get('/delete_:type/:id', { :controller => 'all', :action => 'delete' })
 get('/delete_:type/:id', { :controller => 'all', :action => 'delete' })
 
+get('/update_:type/:id', { :controller => 'all', :action => 'update' })
+get('/update_:type/:id', { :controller => 'all', :action => 'update' })
+get('/update_:type/:id', { :controller => 'all', :action => 'update' })
+
+get('/:type/new_form', { :controller => 'all', :action => 'add' })
+get('/create_:type', { :controller => 'all', :action => 'insert' })
+get('/:type/:id/edit_form', { :controller => 'all', :action => 'edit' })
 #expect type to be directors,movies,actors
 get('/:type', { :controller => 'all', :action => 'index' })
 get('/:type/:id', { :controller => 'all', :action => 'show' })
