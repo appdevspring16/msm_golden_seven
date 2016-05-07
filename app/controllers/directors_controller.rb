eea1e236439b7392c58408a86c48966a5f27864f
Shana Dunnigan <dunniga5@users.noreply.github.com>
2016-05-07 12:13:32 -0500

    final commit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -4,40 +4,38 @@ class DirectorsController < ApplicationController
     @list_of_directors = Director.all
   end
 
-#   def new_form
-#
-#   end
-#   def create_movie
-#     m = Movie.new
-#     m.title = params[:movie_title]
-#     m.year = params[:movie_year]
-#     m.duration = params[:movie_duration]
-#     m.description = params[:movie_description]
-#     m.image_url = params[:movie_url]
-#     m.save
-#     redirect_to("/movies")
-#   end
+  def new_form
+
+  end
+  def create_director
+    d= Director.new
+    d.name = params[:director_name]
+    d.dob = params[:director_dob]
+    d.bio = params[:director_bio]
+    d.image_url = params[:director_url]
+    d.save
+    redirect_to("/directors")
+  end
   def view
     @director=Director.find_by({ :id=> params[:id]})
   end
-#   def destroy
-#     m = Movie.find(params[:id])
-#     m.destroy
-#     redirect_to("/movies")
-#   end
-#   def edit_form
-#   @movie = Movie.find_by({ :id => params[:id]})
-#
-# end
-# def update_row
-#   m = Movie.find_by({ :id => params[:id]})
-#   m.title = params[:movie_title]
-#   m.year = params[:movie_year]
-#   m.duration = params[:movie_duration]
-#   m.description = params[:movie_description]
-#   m.image_url = params[:movie_url]
-#   m.save
-#
-#   redirect_to("/movies/"+params[:id])
-# end
+  def destroy
+    d = Director.find(params[:id])
+    d.destroy
+    redirect_to("/directors")
+  end
+  def edit_form
+  @director = Director.find_by({ :id => params[:id]})
+
+end
+def update_row
+  d = Director.find_by({ :id => params[:id]})
+  d.name = params[:director_name]
+  d.dob = params[:director_dob]
+  d.bio = params[:director_bio]
+  d.image_url = params[:director_url]
+  d.save
+
+  redirect_to("/directors/"+params[:id])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,34 @@ Rails.application.routes.draw do
   # Routes to DELETE Movies
   get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
 
+  # Routes to CREATE Directors
+  get("/directors/new_form", { :controller => "directors", :action => "new_form"})
+  get("/create_director", { :controller => "directors", :action => "create_director"})
+
   # # Routes to READ Directors
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:id", { :controller => "directors", :action => "view" })
+
+  # Routes to EDIT Directors
+  get("/directors/:id/edit", { :controller => "directors", :action => "edit_form" })
+  get("/update_director/:id", { :controller => "directors", :action => "update_row" })
+
+  # Routes to DELETE Directors
+  get("/delete_director/:id", { :controller => "directors", :action => "destroy" })
+
+  # Routes to CREATE Actors
+  get("/actors/new_form", { :controller => "actors", :action => "new_form"})
+  get("/create_actor", { :controller => "actors", :action => "create_actor"})
+
+  # # Routes to READ Actors
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:id", { :controller => "actors", :action => "view" })
+
+  # Routes to EDIT Actors
+  get("/actors/:id/edit", { :controller => "actors", :action => "edit_form" })
+  get("/update_actor/:id", { :controller => "actors", :action => "update_row" })
+
+  # Routes to DELETE Actors
+  get("/delete_actor/:id", { :controller => "actors", :action => "destroy" })
+
 end
