9d8824d8aa926b03cd6df64b9d4ffc31f008d62f
Kenny Moyer <kmoyer@users.noreply.github.com>
2016-05-06 20:09:08 -0400

    kenny-moyer-directors-complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,22 @@
 Rails.application.routes.draw do
 
 #Routes to main page
-get("/",                  { :controller => "movies", :action => "index"})
-
+get("/",                  { :controller => "directors", :action => "index"})
 
 #Routes to CREATE DIRECTORS
-get("/directors/new",     { :controller => "directors", :action => "new_form" })
+get("/directors/new_form",     { :controller => "directors", :action => "new_form" })
 get("/create_director",   { :controller => "directors", :action => "create_row"})
-
 
 #Routes to READ DIRECTORS
 get("/directors",         { :controller => "directors", :action => "index"})
-get("/directors/:id"      { :controller => "directors", :action => "show_details"})
-
-
+get("/directors/:id",      { :controller => "directors", :action => "show_details"})
 
 #Routes to UPDATE DIRECTORS
-
-
-
+get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
 
 #Routes to DESTROY DIRECTORS
-
+get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
 
 
 
