b979ecd6a1ead0832c793710c949cce6bb4e3ca6
deepeshg <deepesh.goyal@gmail.com>
2016-05-07 13:24:14 -0400

    commit

---
+++ db/migrate/20160507171936_create_instructors.rb
@@ -0,0 +1,9 @@
+class CreateInstructors < ActiveRecord::Migration
+  def change
+    create_table :instructors do |t|
+      t.string :first_name
+
+      t.timestamps null: false
+    end
+  end
+end
