bc80cd67b8fa46b12dee928c0979305db1544d11
Shana Dunnigan <dunniga5@users.noreply.github.com>
2016-05-06 21:08:58 -0500

    Added Movies and Directors Index/View

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,43 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+  end
+
+  def new_form
+
+  end
+  def create_movie
+    m = Movie.new
+    m.title = params[:movie_title]
+    m.year = params[:movie_year]
+    m.duration = params[:movie_duration]
+    m.description = params[:movie_description]
+    m.image_url = params[:movie_url]
+    m.save
+    redirect_to("/movies")
+  end
+  def view
+    @movie=Movie.find_by({ :id=> params[:id]})
+  end
+  def destroy
+    m = Movie.find(params[:id])
+    m.destroy
+    redirect_to("/movies")
+  end
+  def edit_form
+  @movie = Movie.find_by({ :id => params[:id]})
+
+end
+def update_row
+  m = Movie.find_by({ :id => params[:id]})
+  m.title = params[:movie_title]
+  m.year = params[:movie_year]
+  m.duration = params[:movie_duration]
+  m.description = params[:movie_description]
+  m.image_url = params[:movie_url]
+  m.save
+
+  redirect_to("/movies/"+params[:id])
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  
+  get("/", { :controller => "movies", :action => "index" })
+
+  # Routes to CREATE Movies
+  get("/movies/new_form", { :controller => "movies", :action => "new_form"})
+  get("/create_movie", { :controller => "movies", :action => "create_movie"})
+
+  # Routes to READ Movies
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:id", { :controller => "movies", :action => "view" })
+
+
+
+  # Routes to EDIT Movies
+  get("/movies/:id/edit", { :controller => "movies", :action => "edit_form" })
+  get("/update_movie/:id", { :controller => "movies", :action => "update_row" })
+
+  # Routes to DELETE Movies
+  get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
+
+  # # Routes to READ Directors
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:id", { :controller => "directors", :action => "view" })
 end
