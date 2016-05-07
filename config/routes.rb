eaf002cc2d110fa1fe77f795d1e83c6b62ac3a5f
mmuntazir <mehdi.muntazir@outlook.com>
2016-05-07 01:37:25 -0500

    Working model - no styling

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get('/create_movie', { :controller => 'movies', :action => 'create_row'})
   get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
   get('/create_director', { :controller => 'directors', :action => 'create_row' })
+  get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
+  get('/create_actor', { :controller => 'actors', :action => 'create_row' })
 
 
   # Reading
@@ -14,6 +16,8 @@ Rails.application.routes.draw do
   get('/movies', { :controller => 'movies', :action => 'index'})
   get('/directors', { :controller => 'directors', :action => 'index'})
   get('/directors/:id', { :controller => 'directors', :action => 'show'})
+  get('/actors', { :controller => 'actors', :action => 'index' })
+  get('/actors/:id', { :controller => 'actors', :action => 'show' })
 
 
 
@@ -22,12 +26,15 @@ Rails.application.routes.draw do
   get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
   get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
   get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
-
+  get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
+  get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
+  
 
 
   # Delete
   get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' })
   get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+  get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })
 
 
 end
