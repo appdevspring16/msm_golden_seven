e4e1c6a478062a4313bda2563e71fba9b2eea60b
Starr Marcello <starr.marcello@users.noreply.github.com>
2016-05-07 11:48:02 -0500

    stopping point 3

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -23,12 +23,11 @@ class DirectorsController < ApplicationController
   end
 
   def edit_form
-    @director = Director.find(params[:id])
+    @director = Director.find_by({ :id=> params[:id] })
   end
 
   def update_row
-    @director = Director.find(params[:id])
-
+    @director = Director.find_by({ :id=> params[:id] })
     @director.dob = params[:dob]
     @director.name = params[:name]
     @director.bio = params[:bio]
