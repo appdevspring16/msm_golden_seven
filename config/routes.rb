6d374c285661fb6b964379c0835ad185d50f18c6
Matthew Keenan <mkeenan@chicagobooth.edu>
2016-05-06 20:18:39 -0500

    New form RCAV

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,24 @@ Rails.application.routes.draw do
   get("/", { :controller => "movies", :action => "index"})
 
   #movies
+  get("/movies/new_form", { :controller => "movies", :action => "new_form"})
+
   get("/movies", { :controller => "movies", :action => "index"})
   get("/movies/:id", { :controller => "movies", :action => "show"})
 
   get("/delete_movie/:id", { :controller => "movies", :action => "destroy"})
 
   #directors
+  get("/directors/new_form", { :controller => "directors", :action => "new_form"})
+
   get("/directors", { :controller => "directors", :action => "index"})
   get("/directors/:id", { :controller => "directors", :action => "show"})
 
   get("/delete_director/:id", { :controller => "directors", :action => "destroy"})
 
   #actors
+  get("/actors/new_form", { :controller => "actors", :action => "new_form"})
+
   get("/actors", { :controller => "actors", :action => "index"})
   get("/actors/:id", { :controller => "actors", :action => "show"})
 
