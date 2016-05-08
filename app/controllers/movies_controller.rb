a61b1e1929cd569232cc303c7b07465a27e0c6a9
rbickham <rbickham@chicagobooth.edu>
2016-05-07 20:29:39 -0500

    rcb-msm-golden-seven-complete

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,50 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+
+  def show
+    @movie = Movie.find_by({ :id => params[:id]})
+  end
+
+  def new_form
+  end
+
+  def create_row
+    movie = Movie.new
+    movie.title = params[:the_title]
+    movie.year = params[:the_year]
+    movie.duration = params[:the_duration]
+    movie.description = params[:the_description]
+    movie.image_url = params[:the_image_url]
+
+    movie.save
+
+    redirect_to("http://localhost:3000/movies")
+  end
+
+  def destroy
+    movie = Movie.find_by({ :id => params[:id]})
+    movie.destroy
+    redirect_to("http://localhost:3000/movies")
+  end
+
+  def edit_form
+    @movie = Movie.find(params[:id])
+  end
+
+  def update_row
+    movie = Movie.find_by({ :id => params[:id]})
+    movie.title = params[:the_title]
+    movie.year = params[:the_year]
+    movie.duration = params[:the_duration]
+    movie.description = params[:the_description]
+    movie.image_url = params[:the_image_url]
+
+    movie.save
+
+    redirect_to("http://localhost:3000/movies/#{movie.id}")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,55 @@
 Rails.application.routes.draw do
-  
+
+  #DIRECTORS
+
+  #Routes to CREATE directors
+  get("/directors/new",     { :controller => "directors", :action => "new_form" })
+  get("/create_director", { :controller => "directors", :action => "create_row" })
+
+  #Routes to EDIT directors
+  get("/directors/:id/edit", { :controller => "directors", :action => "edit_form" })
+  get("/update_director/:id", { :controller => "directors", :action => "update_row" })
+
+  # Routes to READ directors
+  get("/directors",           { :controller => "directors", :action => "index" })
+  get("/directors/:id",       { :controller => "directors", :action => "show" })
+
+  #Routes to DELETE directors
+  get("/delete_director/:id", { :controller => "directors", :action => "destroy" })
+
+  #ACTORS
+
+  #Routes to CREATE actors
+  get("/actors/new",     { :controller => "actors", :action => "new_form" })
+  get("/create_actor", { :controller => "actors", :action => "create_row" })
+
+  #Routes to EDIT actors
+  get("/actors/:id/edit", { :controller => "actors", :action => "edit_form" })
+  get("/update_actor/:id", { :controller => "actors", :action => "update_row" })
+
+  # Routes to READ actors
+  get("/actors",           { :controller => "actors", :action => "index" })
+  get("/actors/:id",    { :controller => "actors", :action => "show" })
+
+  #Routes to DELETE actors
+  get("/delete_actor/:id", { :controller => "actors", :action => "destroy" })
+
+
+  #MOVIES
+
+  #Routes to CREATE movies
+  get("/movies/new",     { :controller => "movies", :action => "new_form" })
+  get("/create_movie", { :controller => "movies", :action => "create_row" })
+
+  #Routes to EDIT movies
+  get("/movies/:id/edit", { :controller => "movies", :action => "edit_form" })
+  get("/update_movie/:id", { :controller => "movies", :action => "update_row" })
+
+  # Routes to READ movies
+  get("/movies",           { :controller => "movies", :action => "index" })
+  get("/movies/:id",    { :controller => "movies", :action => "show" })
+
+  #Routes to DELETE movies
+  get("/delete_movie/:id", { :controller => "movies", :action => "destroy" })
+
 end
