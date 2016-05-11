9964cd404ac85672cd3c1631c24cb00ff56bdd77
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-05-11 17:49:34 -0500

    Stopping point_7
    Directors:
    -Delete function not working
    -Update row function not working

--- app/controllers/concerns/directors_controller.rb
+++ app/controllers/concerns/directors_controller.rb
@@ -25,24 +25,22 @@ class DirectorsController < ApplicationController
     @director.save
 
     render "show"
-    
+  end
 
-    def edit_form
-      @director =Director.find(params[:id])
-    end
+  def edit_form
+    @director =Director.find(params[:id])
+  end
 
-    def update_row
-      @director = Director.find(params[:id])
+  def update_row
+    @director = Director.find(params[:id])
 
-      @director.dob = params[:dob]
-      @director.name = params[:name]
-      @director.bio = params[:bio]
-      @director.image_url = params[:image_url]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
-      @director.save
+    @director.save
 
-      render("show")
-
-    end
+    render("show")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ get("/create_director", { :controller => "directors", :action => "create_row"} )
 get("/directors", { :controller => "directors", :action => "index" })
 get("/directors/:id", { :controller => "directors", :action => "show"})
 
-#Delete
-get("/delete_director/:id", { :controller => "directors", :action => "destroy"})
-
 #Update
 get("/directors/:id/edit_form", { :controller => "directors", :action => "edit_form"})
 get("/update_director/:id", { :controller => "directors", action => "update_row"})
+
+#Delete
+get("/delete_director/:id", { :controller => "directors", :action => "destroy"})
 end
