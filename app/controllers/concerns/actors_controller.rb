8696993b79f1329319b73cc407f86a8ba7fa31eb
stephenandrewberger <stephen.berger@chicagobooth.edu>
2016-05-12 00:31:44 -0500

    Stopping_point_9
    Finished all actions for Actors, but unable to get them to show up in
    browser

--- app/controllers/concerns/actors_controller.rb
+++ app/controllers/concerns/actors_controller.rb
@@ -7,8 +7,27 @@ def show
 @actor = Actor.find(params[:id])
 end
 
+def destroy
+  @actor_to_destroy = Actor.find(params[:id])
+  @actor_to_destroy.destroy
+end
 
+def new_form
+end
 
+def create_row
+  @actor = Actor.new
+  @actor.dob = params[:dob]
+  @actor.name = params[:name]
+  @actor.bio = params[:bio]
+  @actor.image_url = params[:image_url]
 
+  @actor.save
+  render "show"
+end
+
+def edit_form
+  @actor = Actor.find(params[:id])
+end
 
 end
--- app/controllers/concerns/directors_controller.rb
+++ app/controllers/concerns/directors_controller.rb
@@ -28,11 +28,11 @@ class DirectorsController < ApplicationController
   end
 
   def edit_form
-    @director =Director.find(params[:id])
+    @director = Director.find(params[:id])
   end
 
   def update_row
-    @director = Director.find(params[:id])
+    @direc  tor = Director.find(params[:id])
 
     @director.dob = params[:dob]
     @director.name = params[:name]
